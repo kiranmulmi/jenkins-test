8cde3284ea8830877c25941a08ca35314517dccc
kiran mulmi <kiran.mulmi@gmail.com>
2021-08-17 17:36:34 +0545

    abc

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const app = express();
 app.get('/', function (req, res) {
     res.send('hello world 2345');
 });
+app.get('/kiran', function (req, res) {
+    res.json({
+        success: true,
+        message: "good very good"
+    });
+});
 app.listen(process.env.PORT || 3000);
 
 module.exports = app;
