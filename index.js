251d36ad1b18917a06dd4bd555f4e4d8cb38b564
kiran mulmi <kiran.mulmi@gmail.com>
2021-08-17 10:59:03 +0545

    var to const rename

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 const app = express();
-//Respond with "hello world" for requests that hit our root "/"
+//Respond with "hello world" for requests that hit our root
 app.get('/', function (req, res) {
     res.send('hello world 12');
 });
