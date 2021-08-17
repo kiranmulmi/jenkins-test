2ce9070119fdde4132f6f7c6a850bf77636c7ebb
kiran mulmi <kiran.mulmi@gmail.com>
2021-08-17 17:33:32 +0545

    abc

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-    res.send('hello world 234');
+    res.send('hello world 2345');
 });
 app.listen(process.env.PORT || 3000);
 
--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../index.js');
 describe('GET /', function() {
     it('respond with hello world', function(done) {
         //navigate to root and check the the response is "hello world"
-        request(app).get('/').expect('hello world 234', done);
+        request(app).get('/').expect('hello world 2345', done);
     });
 });
